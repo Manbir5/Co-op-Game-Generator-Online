f9d5039111d2d1a2ba11308cedc808b1af504086
Manbir5 <72167868+Manbir5@users.noreply.github.com>
2021-11-11 02:14:06 -0500

    commit

--- game.php
+++ game.php
@@ -21,23 +21,23 @@ if (isset($_GET["console"]) and isset($_GET["type"]) and isset($_GET["type"])){
 	$dbname="games_for_game_hoard";
 	$yourfield = "genre";
 	
-	$conn = mysqli_connect($hostname,$username, $password) or die ("<script language='JavaScript'>alert('Error: Unable to connect to database.'),history.go(-1)</script>");
-	mysqli_select_db($conn,$dbname);
+	#$conn = mysqli_connect($hostname,$username, $password) or die ("<script language='JavaScript'>alert('Error: Unable to connect to database.'),history.go(-1)</script>");
+	#mysqli_select_db($conn,$dbname);
 	
 	# Check If Record Exists
 	
 	# $query = "SELECT * FROM $usertable";
-  $query = "SELECT gc.game_title FROM (SELECT * FROM Games INNER JOIN Games_Consoles ON Games.id = Games_Consoles.id INNER JOIN Consoles ON Consoles.cid = Games_Consoles.cid) as gc";
+  #$query = "SELECT gc.game_title FROM (SELECT * FROM Games INNER JOIN Games_Consoles ON Games.id = Games_Consoles.id INNER JOIN Consoles ON Consoles.cid = Games_Consoles.cid) as gc";
 	
-	$result = mysqli_query($conn,$query);
+	#$result = mysqli_query($conn,$query);
 	
-	if($result){
-		while($row = mysqli_fetch_array($result)){
-			$name = $row;
+	#if($result){
+	#	while($row = mysqli_fetch_array($result)){
+	#		$name = $row;
 			#echo "Name: ".$name."<br/>";
 
-    }
-		}
+  #  }
+	#	}
 	}
 else{
 	$invalid = "invalid";
@@ -51,8 +51,8 @@ if ($number == 1)
 	$website = "https://en.wikipedia.org/wiki/Borderlands_2";
 }
 else{
-	$game = "A Way Out";
-	$website = "https://en.wikipedia.org/wiki/A_Way_Out_(video_game)";
+	$game = "OverCooked 2";
+	$website = "https://en.wikipedia.org/wiki/Overcooked_2";
 }
 
 ?>
@@ -65,12 +65,31 @@ $myfile = fopen("images.txt", "w");
 $url = "\n".$website;
 fwrite($myfile,$url);
 fclose($myfile);
+sleep(1);
 foreach(file("images.txt") as $line){
 }
-$line = $image_title."jpg"
+$line = $line.".png";
+
+$image_transformer = fopen(".//images/image_transformer_pipe.txt", "w");
+$measurements = 'IN | 200,300,"'.$line.'"';
+fwrite($image_transformer,$measurements);
+fclose($image_transformer);
+sleep(1);
+foreach(file("./images/image_transformer_pipe.txt") as $output){
+}
+$output = substr($output,7);
+$output = substr($output,0,-1);
+
 ?>
 <div>
-<img src = <?php echo "images/".$line ?>>
+<img class="center" src = <?php echo "images/".$output ?>>
 </div>
+<br />
+<br />
+<form action ="./game.php" method="get">
+<div class ="submit">
+<button type ="submit"> Re-Submit </button>
+</div>
+</form>
 </body>
 </html> 
